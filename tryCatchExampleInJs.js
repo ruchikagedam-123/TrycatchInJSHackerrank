ed0de6074d8046e7a0adc6e1482bf68b99d05096
ruchikagedam-123 <67160652+ruchikagedam-123@users.noreply.github.com>
2021-01-09 00:29:05 +0530

    try catch example updated successfully.

---
+++ tryCatchExampleInJs.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ *  * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function reverseString(s) {
+        try {
+         s = s.split('').reverse().join('');
+    } catch (e) {
+         console.log('s.split is not a function');
+    }
+
+    console.log(s);
+}
+
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
